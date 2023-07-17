7fd8c0de712f87f0cc0d622878bbba2abedd0879
codeboxxx <clintonsilvanus@gmail.com>
2023-07-17 21:51:08 +0100

    arithmetic with vectors, selection of vectors by position and name

--- refresing R.R
+++ refresing R.R
@@ -19,3 +19,45 @@ names(roulette_vector) <- days_vector
 
 #print out the vectors
 poker_vector
+
+#arithmetic with vectors
+#calculating wins and losses on each day
+total_daily <- poker_vector + roulette_vector
+total_daily
+
+#total win/loss for each game
+total_poker <- sum(poker_vector)
+total_roulette <- sum(roulette_vector)
+
+#total week win/loss 
+total_week <- sum(total_daily)
+total_week
+
+#selection of elements in a vector
+poker_midweek <- poker_vector[2:4]
+poker_midweek
+
+#another way
+poker_midweek2 <- poker_vector[c(2,3,4)]
+
+roulette_selection_vector <- roulette_vector[2:5]
+
+#selection of elements in a vector by name
+poker_start <- poker_vector[c("Monday","Tuesday","Wednesday")]
+poker_start
+
+#calculate the mean of the selected vectors
+mean(poker_start)
+
+
+
+
+
+
+
+
+
+
+
+
+
